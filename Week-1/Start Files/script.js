55e52a314250e329223134678095986a96562015
Aditya Sanapala <wickedsan106@gmail.com>
2024-12-12 14:08:54 +0530

    Add Start Files

---
+++ Week-1/Start Files/script.js
@@ -0,0 +1,5 @@
+// Sticky Navbar: Adds 'sticky' class when the navbar reaches the top of the page
+
+// Smooth Scrolling: Scrolls smoothly to the target section when a button is clicked
+
+// Contact Form Validation: Checks if all fields are filled before submitting and shows success or error message
